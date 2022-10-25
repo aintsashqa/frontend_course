a1de1830b2448b3618b6bba57093333a3c0e51f9
aintsashqa <ujeromi@gmail.com>
2022-10-25 20:22:37 +0300

    add: js table/edit-form

---
+++ js/table.js
@@ -0,0 +1,26 @@
+const contact1 = {
+  firstName: "John",
+  lastName: "Rambo",
+  phoneNumber: "+7 (999) 999-99-99",
+  email: "johnrambo@gmail.com",
+  companyName: "U.S.A. Army",
+  jobTitle: "Soldier"
+};
+
+const contact2 = {
+  firstName: "Sara",
+  lastName: "Conor",
+  phoneNumber: "+7 (901) 123-45-67",
+  email: "saraconor@gmail.com",
+  companyName: "Cyberdyne",
+  jobTitle: "Robot destroyer"
+};
+
+const contact3 = {
+  firstName: "James",
+  lastName: "Bond",
+  phoneNumber: "+7 (000) 765-43-21",
+  email: "jamesbond@gmail.com",
+  companyName: "MI6",
+  jobTitle: "Agent 007"
+};
