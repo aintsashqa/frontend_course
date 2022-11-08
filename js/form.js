a15c54753666f87afabd8b1e9e964fea2c39c500
aintsashqa <ujeromi@gmail.com>
2022-11-08 20:37:34 +0300

    upd: js form events

--- js/form.js
+++ js/form.js
@@ -8,3 +8,23 @@ const contact = {
 };
 
 console.log(`${contact.firstName} ${contact.lastName}, ${contact.jobTitle}`);
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("first_name").setAttribute("value", contact.firstName);
+  document.getElementById("last_name").setAttribute("value", contact.lastName);
+  document.getElementById("phone_number").setAttribute("value", contact.phoneNumber);
+  document.getElementById("email").setAttribute("value", contact.email);
+  document.getElementById("company_name").setAttribute("value", contact.companyName);
+  document.getElementById("job_title").setAttribute("value", contact.jobTitle);
+
+  const contactForm = document.querySelector("form");
+  const successBlock = document.querySelector(".form-success");
+
+  const handleSubmitButtonEvent = (e) => {
+    e.preventDefault();
+    contactForm.classList.add("hide");
+    successBlock.classList.remove("hide")
+  };
+
+  contactForm.addEventListener("submit", handleSubmitButtonEvent);
+});
